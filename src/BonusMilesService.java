f23300afc0a7378b368278dca4ac743cf18b8b69
Ilona Tatarenko <ilona-tatarenko@mail.ru>
2022-10-11 13:15:21 +0300

    Initial commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+    public int calculate ( int cost) {
+     int miles;
+     int price;
+     cost = 20;
+     price = 10000;
+     miles = price / cost;
+     return miles;
+    }
+
+}
